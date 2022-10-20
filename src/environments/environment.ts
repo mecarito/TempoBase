45ba70a1db2d626b9af880913c5d3ae80e8ec8bb
mecarito <pieritomecar@gmail.com>
2022-10-20 15:09:35 +0300

    added some environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl: 'https://api.spotify.com/v1',
+
+  // Intentionally displaying this here. Created separate spotify account
+  apiBasicToken: 'Basic MWIwYTIyODBmMWZiNDliMjg2NDI1MTU4OWQyMDM4OTc6NTlkYzQ0NmRkZWRjNDY2NGI5NzlmMzVjMzk0MmZmYTI',
+  apiClientId: '1b0a2280f1fb49b2864251589d203897'
+
 };
 
 /*
