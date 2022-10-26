f077637abed25ed0a53c51b3b96a8076665e31ac
mecarito <pieritomecar@gmail.com>
2022-10-26 14:50:35 +0300

    sidebar styling

--- src/app/modules/dashboard/sidebar/sidebar.component.ts
+++ src/app/modules/dashboard/sidebar/sidebar.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
+  menus = [
+    {
+      name: 'Home',
+      imageSrc: 'assets/icons/home.svg',
+      link: '',
+    },
+    {
+      name: 'Albums',
+      imageSrc: 'assets/icons/book.svg',
+      link: '',
+    },
+    {
+      name: 'Artist',
+      imageSrc: 'assets/icons/user.svg',
+      link: '',
+    },
+    {
+      name: 'Discover',
+      imageSrc: 'assets/icons/search.svg',
+      link: '',
+    },
+  ];
+  playlists = [
+    {
+      name: 'Recent',
+      imageSrc: 'assets/icons/clock.svg',
+      link: '',
+    },
+    {
+      name: 'Favorites',
+      imageSrc: 'assets/icons/heart.svg',
+      link: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-purple": "#6868FF",
+        "dark-gray": "#7F8B9C",
+        "main-content-bg": "#F6F8F9",
+      },
+      screens: {
+        lg: "1280px",
+        lgxl: "1366px",
+        lgxxl: "1920px",
+      },
+    },
   },
   plugins: [],
 };
