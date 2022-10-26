6194ca55b49a0f1e50d5012114421897e47325f1
mecarito <pieritomecar@gmail.com>
2022-10-27 00:05:06 +0300

    created slider component

--- src/app/modules/shared/components/player/player.component.ts
+++ src/app/modules/shared/components/player/player.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss']
+  styleUrls: ['./player.component.scss'],
 })
 export class PlayerComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    let slider = document.getElementById('track-time');
+    if (slider) {
+      slider.style.backgroundSize = '20% 100%';
+      console.log(slider.style.backgroundSize);
+    }
   }
 
+  handleInputChange(e: Event) {
+    let target = e.target as HTMLInputElement;
+    const min = Number(target.min);
+    const max = Number(target.max);
+    const val = Number(target.value);
+
+    target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
+  }
 }
