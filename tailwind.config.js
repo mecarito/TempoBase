f2a6f614afa6eb8064d5369e26507061ec4e98e7
mecarito <pieritomecar@gmail.com>
2022-11-01 12:18:20 +0300

    updated branding colors, changed the favicon and updated welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "primary-purple": "#6868FF",
-        "dark-gray": "#7F8B9C",
-        "main-content-bg": "#F6F8F9",
-        "gradient-one": "#3f8fff",
-        "gradient-two": "#9244ff",
-        "light-gray": "#F5F7F8",
+        "spotify-black": "#191414",
+        "spotify-green": "#1DB954",
+      },
+      fontFamily: {
+        Roboto: "'Roboto', sans-serif",
       },
       screens: {
         lg: "1280px",
