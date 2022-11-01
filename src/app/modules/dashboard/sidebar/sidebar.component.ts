77253380cc788d4688cf498cffa4ba47b7336e62
mecarito <pieritomecar@gmail.com>
2022-11-01 14:31:26 +0300

    deleted all icons and updated the sidebar

--- src/app/modules/dashboard/sidebar/sidebar.component.ts
+++ src/app/modules/dashboard/sidebar/sidebar.component.ts
@@ -6,28 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  menus = [
-    {
-      name: 'Home',
-      imageSrc: 'assets/icons/home.svg',
-      link: 'home',
-    },
-    {
-      name: 'Albums',
-      imageSrc: 'assets/icons/book.svg',
-      link: 'albums',
-    },
-    {
-      name: 'Artist',
-      imageSrc: 'assets/icons/user.svg',
-      link: 'artists',
-    },
-    {
-      name: 'Discover',
-      imageSrc: 'assets/icons/search.svg',
-      link: 'discover',
-    },
-  ];
 
   constructor() {}
 
