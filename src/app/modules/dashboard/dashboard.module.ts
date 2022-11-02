baca0b1495b82b4fc44c4bf285e306519bd610e2
mecarito <pieritomecar@gmail.com>
2022-11-02 16:29:59 +0300

    deleted home, albums, artists, playlists, discover components and updated dashboard module

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -4,38 +4,16 @@ import { DashboardComponent } from './dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '../shared/shared.module';
-import { HomeComponent } from './home/home.component';
-import { AlbumsComponent } from './albums/albums.component';
-import { ArtistsComponent } from './artists/artists.component';
-import { DiscoverComponent } from './discover/discover.component';
-import { RecentPlayedComponent } from './recent-played/recent-played.component';
-import { PlaylistsComponent } from './playlists/playlists.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    children: [
-      { path: 'home', component: HomeComponent },
-      { path: 'albums', component: AlbumsComponent },
-      { path: 'artists', component: ArtistsComponent },
-      { path: 'discover', component: DiscoverComponent },
-      { path: 'played-recently', component: RecentPlayedComponent },
-      { path: 'playlist', component: PlaylistsComponent }
-    ],
+    children: [],
   },
 ];
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    SidebarComponent,
-    HomeComponent,
-    AlbumsComponent,
-    ArtistsComponent,
-    DiscoverComponent,
-    RecentPlayedComponent,
-    PlaylistsComponent,
-  ],
+  declarations: [DashboardComponent, SidebarComponent],
   imports: [CommonModule, SharedModule, RouterModule.forChild(routes)],
 })
 export class DashboardModule {}
