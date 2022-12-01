adfd35ffc0e26ae1b4943928804c4c1aed5f1e7a
mecarito <pieritomecar@gmail.com>
2022-12-01 18:54:39 +0300

    updated audio and volume change sliders

--- src/app/modules/shared/components/player/player.component.ts
+++ src/app/modules/shared/components/player/player.component.ts
@@ -18,15 +18,19 @@ export class PlayerComponent implements OnInit, AfterViewInit {
   muted = false;
   favorite = false;
 
-  audioDuration!: number
+  audioDuration!: number;
+  currentTime: number = 0;
 
   @ViewChild('audio') audio!: ElementRef<HTMLAudioElement>;
+  @ViewChild('audioSlider') audioSlider!: ElementRef<HTMLInputElement>;
+  @ViewChild('volumeSlider') volumeSlider!: ElementRef<HTMLInputElement>;
+
   constructor() {}
 
   ngOnInit(): void {
     let slider = document.getElementById('track-time');
     if (slider) {
-      slider.style.backgroundSize = '20% 100%';
+      slider.style.backgroundSize = '0 100%';
     }
 
     let volume = document.getElementById('volume-level');
@@ -37,13 +41,8 @@ export class PlayerComponent implements OnInit, AfterViewInit {
     // const audio = document.querySelector('audio');
 
     // if (audio) {
-    //   audio.addEventListener('loadedmetadata', () => {
-    //       console.log(audio.duration, this.audio.nativeElement.duration)
-    //   });
-    // }
-
-    // if (this.audio) {
-    //   this.audio.nativeElement.onloadedmetadata()
+    //   const bufferedAmount = audio.buffered.end(audio.buffered.length - 1);
+    //   this.currentTime = audio.currentTime
     // }
   }
 
@@ -52,33 +51,53 @@ export class PlayerComponent implements OnInit, AfterViewInit {
   }
 
   onLoadedMetaData(audio: HTMLAudioElement) {
-    this.audioDuration = audio.duration
-     console.log(audio.duration)
+    this.audioDuration = audio.duration;
+    // this.audioSlider.nativeElement.max = String(Math.round(audio.duration));
+    const bufferedAmount = audio.buffered.end(audio.buffered.length - 1);
+    const seekableAmount = audio.seekable.end(audio.seekable.length - 1);
+    // console.log(audio.duration, bufferedAmount, seekableAmount);
   }
 
-  handleInputChange(e: Event) {
-    let target = e.target as HTMLInputElement;
-    const min = Number(target.min);
-    const max = Number(target.max);
-    const val = Number(target.value);
+  onPlaying(audio: HTMLAudioElement) {
+    // console.log(audio.currentTime)
+    this.currentTime = audio.currentTime;
+  }
 
-    target.style.backgroundSize = ((val - min) * 100) / (max - min) + '% 100%';
+  onDurationChange(audio: HTMLAudioElement) {
+    const bufferedAmount = audio.buffered.end(audio.buffered.length - 1);
+    const seekableAmount = audio.seekable.end(audio.seekable.length - 1);
+
+    console.log(bufferedAmount, seekableAmount);
+  }
+
+  audioSliderChange() {
+    const val = this.audioSlider.nativeElement.value;
+    this.audioSlider.nativeElement.style.backgroundSize = `${val}% 100%`;
+  }
+
+  volumeSliderChange() {
+    const val = this.volumeSlider.nativeElement.value;
+    this.volumeSlider.nativeElement.style.backgroundSize = `${val}% 100%`;
   }
 
   play() {
     this.playing = true;
+    this.audio.nativeElement.play();
   }
 
   pause() {
     this.playing = false;
+    this.audio.nativeElement.pause();
   }
 
   mute() {
     this.muted = true;
+    this.audio.nativeElement.muted = true;
   }
 
   unMute() {
     this.muted = false;
+    this.audio.nativeElement.muted = false;
   }
 
   addToFavorite() {
